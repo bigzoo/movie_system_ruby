6eee98d154a2442b232f2663f40586365961a63c
Chris Nyaga <nyagachris75@gmail.com>
2017-07-12 17:53:11 +0300

    App.rb file created and required gems added and connection to the database initiated.

---
+++ app.rb
@@ -0,0 +1,9 @@
+require("sinatra")
+  require("sinatra/reloader")
+  also_reload("lib/**/*.rb")
+  require("./lib/actor")
+  require("./lib/movie")
+  require("pg")
+
+  DB = PG.connect({:dbname => "movie_database"})
+
