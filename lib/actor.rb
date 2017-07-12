93c69da916e6d132c52409fd2d9552047c3b057d
Chris Nyaga <nyagachris75@gmail.com>
2017-07-12 17:55:03 +0300

    Class method .all created to fetch all actors from the Database.

--- lib/actor.rb
+++ lib/actor.rb
@@ -6,4 +6,15 @@ class Actor
     @id = attributes.fetch(:id)
   end
 
+  define_singleton_method(:all) do
+    returned_actors = DB.exec('SELECT * FROM actors;')
+    actors = []
+    returned_actors.each do |actor|
+      name = actor.fetch('name')
+      id = actor.fetch('id').to_i
+      actors.push(Actor.new(name: name, id: id))
+    end
+    actors
+  end
+
 end
