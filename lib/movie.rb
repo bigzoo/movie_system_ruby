e8bfa68582424bd64cf260b07922021342429663
Chris Nyaga <nyagachris75@gmail.com>
2017-07-12 18:35:38 +0300

    Attribute reader added to generate getters fot various parameters and an initialize method to create new instances of the class with the specific required parameters.

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,2 +1,9 @@
 class Movie
+    attr_reader(:name, :id)
+
+    define_method(:initialize) do |attributes|
+      @name = attributes.fetch(:name)
+      @id = attributes.fetch(:id)
+    end
+
   end
