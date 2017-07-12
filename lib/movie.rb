e1864f2b5d1fb88d18e1eebbd8128b34d10d1f2f
Chris Nyaga <nyagachris75@gmail.com>
2017-07-12 18:36:15 +0300

    Class method .all created to fetch all movies from the database.

--- lib/movie.rb
+++ lib/movie.rb
@@ -6,4 +6,15 @@ class Movie
       @id = attributes.fetch(:id)
     end
 
+    define_singleton_method(:all) do
+      returned_movies = DB.exec("SELECT * FROM movies;")
+      movies = []
+      returned_movies.each() do |movie|
+        name = movie.fetch("name")
+        id = movie.fetch("id").to_i()
+        movies.push(Movie.new({:name => name, :id => id}))
+      end
+      movies
+    end
+
   end
